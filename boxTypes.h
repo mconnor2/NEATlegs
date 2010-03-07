a53d557372756f44b80e05f7fcef50ecb22346d1
mconnor <mconnor@202b6e8a-0321-0410-ab3f-db4cf95c4ef8>
2010-03-07 20:49:45 +0000

    Moved all box2d typedefs to one header
    git-svn-id: svn://mc2net.gotdns.com/repos/legs/trunk@572 202b6e8a-0321-0410-ab3f-db4cf95c4ef8

---
+++ boxTypes.h
@@ -0,0 +1,35 @@
+#ifndef __BOX_TYPE_H__
+#define __BOX_TYPE_H__
+
+#include <Box2D.h>
+#include <vector>
+#include <map>
+
+#include <boost/shared_ptr.hpp>
+
+class World;
+class Creature;
+class Muscle;
+
+typedef b2Vec2 Vec2;
+
+typedef b2Shape Shape;
+
+typedef b2Body Body;
+typedef boost::shared_ptr<Body> BodyP;
+typedef std::vector<BodyP> bodyList;
+
+typedef b2Joint Joint;
+typedef boost::shared_ptr<Joint> JointP;
+
+typedef b2RevoluteJoint RevoluteJoint;
+typedef boost::shared_ptr<RevoluteJoint> RevoluteJointP;
+typedef std::vector<RevoluteJointP> jointList;
+
+typedef boost::shared_ptr<Muscle> MuscleP;
+typedef std::vector<MuscleP> muscleList;
+
+typedef boost::shared_ptr<Creature> CreatureP;
+typedef std::vector<CreatureP> creatureList;
+
+#endif
